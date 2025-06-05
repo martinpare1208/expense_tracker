53608ce47b759f29a34ac80ede7e80fa31e69c47
Martin Pare <martinapilado@gmail.com>
2025-06-04 21:25:42 -0400

    refactored sidebar into base.html and have main content appear correctly

--- app/routes.py
+++ app/routes.py
@@ -3,4 +3,8 @@ from flask import render_template
 
 @app.route("/")
 def main():
-    return render_template('index.html')
+    return render_template('home.html', active="home")
+
+@app.route("/dashboard")
+def dashboard():
+    return render_template('home.html', active='dashboard')
